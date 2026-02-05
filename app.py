818037ffb785c0d7463f7da2370a484d45f249be
madisonwilliams-dev <madisonwilliams1688@gmail.com>
2026-02-05 16:09:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import webbrowser
 import os
 
 # Get the absolute path to your file
-file_path = os.path.abspath("index.html")
+file_path = os.path.abspath("game.html")
 
 # Open it in a new tab
-webbrowser.open(f"file://{game}")
+webbrowser.open(f"file://{file_path}")
 
 #def load():
 #  print("working")
