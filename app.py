e73b8ab7115ea4c18d315a5ec6cbbc598c997425
madisonwilliams-dev <madisonwilliams1688@gmail.com>
2026-02-05 17:16:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 def home():
     # Flask looks in the /templates folder by default
     # This creates a response object so we can manually set the content type
-    response = make_response(render_template('game.html'))
-    response.headers['Content-Type'] = 'text/html'
-    return response
+    return render_template('index.html')  # Flask handles the "text/html" header for you
+
 
 if __name__ == '__main__':
     # Use the PORT environment variable provided by Render
